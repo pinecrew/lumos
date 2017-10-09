2860358f6f6d794d7b8528fcfe9cce7b421d50f2
Alexey Golubev <dr.freecx@gmail.com>
2017-10-09 10:58:26 +0300

    [upd] implement lumos.py funcs

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,16 @@ use std::fs::File;
 use std::fs::OpenOptions;
 use std::io::prelude::*;
 use std::{thread, time};
-use std::mem;
+use std::io::SeekFrom;
+use std::cmp;
 use tini::Ini;
 
 #[derive(Debug)]
 struct Configuration {
     max_backlight: i32,
-    min_backlight: u32,
-    min_illuminance: u32,
-    max_illuminance: u32,
+    min_backlight: i32,
+    min_illuminance: i32,
+    max_illuminance: i32,
     backlight: File,
     illuminance: File,
 }
@@ -53,27 +54,58 @@ impl Configuration {
         }
     }
 
-    fn set(&mut self, value: u32) {
-        if value >= self.min_backlight && value <= self.max_backlight as u32 {
-            write!(self.backlight, "{}", value);
+    fn set(&mut self, value: i32) {
+        if value >= self.min_backlight && value <= self.max_backlight as i32 {
+            self.backlight.seek(SeekFrom::Start(0)).ok();
+            write!(self.backlight, "{}", value).ok();
         }
     }
 
-    fn get(&mut self) -> u32 {
-        let mut buffer = [0; 4];
-        self.illuminance.read(&mut buffer[..]);
-        unsafe {
-            mem::transmute::<[u8; 4], u32>(buffer)
+    fn get(&mut self) -> i32 {
+        let mut buffer = String::new();
+        self.illuminance.seek(SeekFrom::Start(0)).ok();
+        self.illuminance.read_to_string(&mut buffer).ok();
+        match buffer.trim().parse::<i32>() {
+            Ok(value) => value,
+            Err(_) => panic!("can't parse `{}` value", buffer)
         }
     }
+
+    fn lumos_to_backlight(&self, v: i32) -> i32 {
+        let x = (v - self.min_illuminance) / (self.max_illuminance - self.min_illuminance);
+        (self.min_backlight + (self.max_backlight - self.min_backlight) * ((2 - x) * x).pow(2)) as i32
+    }
+}
+
+fn transition(x: f32, center: f32, range: f32) -> f32 {
+    1.0 / ((15.0 * (x - center) / range).exp() + 1.0)
 }
 
 fn main() {
     let sleep_time = time::Duration::from_millis(100);
+    let step_time = time::Duration::from_millis(50);
     let mut config = Configuration::init("config.ini");
+    let mut start = 0;
+    let mut end = 0;
     loop {
-        let val = config.get();
-        println!("val = {}", val);
+        start = end;
+        end = config.get();
+        let mut steps = cmp::min(30, (end - start).abs() / 10);
+        println!("steps = {}", steps);
+        if steps > 0 {
+            steps += 5;
+            for i in 0..steps + 1 {
+                let v = ((start - end) as f32 * transition(i as f32, steps as f32 / 2.0, steps as f32)) as i32 + end;
+                println!("v = {}", v);
+                let bv = config.lumos_to_backlight(v);
+                println!("bv = {}", bv);
+                config.set(bv);
+                thread::sleep(step_time);
+            }
+        } else {
+            let value = config.lumos_to_backlight(end);
+            config.set(value);
+        }
         thread::sleep(sleep_time);
     }
 }
